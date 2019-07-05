66c8020a05f41bb76726200dbd9d6433d06467ed
Kelvin Li <kelvindk@gmail.com>
2019-07-05 12:34:03 +0800

    no message

--- gst/pdnraudio/gstpdnraudio.c
+++ gst/pdnraudio/gstpdnraudio.c
@@ -105,8 +105,8 @@ gst_pd_nr_audio_class_init (GstPdNrAudioClass * klass)
       &gst_pd_nr_audio_sink_template);
 
   gst_element_class_set_static_metadata (GST_ELEMENT_CLASS(klass),
-      "FIXME Long name", "Generic", "FIXME Description",
-      "FIXME <fixme@example.com>");
+      "PlayDate audio noise reduction", "Generic", " ",
+      "PlayDate <support@startplaydate.com>");
 
   gobject_class->set_property = gst_pd_nr_audio_set_property;
   gobject_class->get_property = gst_pd_nr_audio_get_property;
@@ -121,17 +121,21 @@ gst_pd_nr_audio_class_init (GstPdNrAudioClass * klass)
 static void
 gst_pd_nr_audio_init (GstPdNrAudio *pdnraudio)
 {
-  // pdnraudio->aFile = fopen( "audio.txt","w" );
-  // pdnraudio->bFile = fopen( "source.txt","w" );
+#ifdef ESTIMATION_MODE
+  pdnraudio->aFile = fopen ("/sdcard/Music/est.txt","w");
+  // pdnraudio->bFile = fopen ("estI.txt","w");
+#endif
 
   pdnraudio->frequeny_sum_count = 0;
-  pdnraudio->frequeny_data = g_new (GstFFTF32Complex, (FFT_WINDOW_SIZE/2)+2);
+  pdnraudio->frequeny_data = g_new (GstFFTF32Complex, FFT_SIZE);
 
-  pdnraudio->in_array = g_new0(gfloat, FFT_WINDOW_SIZE*2);
-  pdnraudio->out_array = g_new0(gfloat, FFT_WINDOW_SIZE*2);
+  pdnraudio->in_array = g_new0 (gfloat, FFT_WINDOW_SIZE*2);
+  pdnraudio->out_array = g_new0 (gfloat, FFT_WINDOW_SIZE*2);
 
+  pdnraudio->attack_count = g_new0 (guint16, FFT_SIZE);
+  pdnraudio->release_count = g_new0 (guint16, FFT_SIZE);
 
-  g_print("gst_pd_nr_audio_init output_queue\n");
+  g_print ("gst_pd_nr_audio_init\n");
 
   pdnraudio->hop_offset = FFT_HOP_SIZE;
 }
@@ -185,26 +189,40 @@ gst_pd_nr_audio_finalize (GObject * object)
 
   g_print("gst_pd_nr_audio_finalize\n");
 
-  // GstFFTF32Complex *freq_data = pdnraudio->frequeny_data;
-  // fprintf(pdnraudio->aFile,"const gfloat estR[] = {");
-  // fprintf(pdnraudio->bFile,"const gfloat estI[] = {");
-  // for(int j=0; j<FFT_SIZE; j++) {
-  //   fprintf(pdnraudio->aFile,"%f ,", freq_data[j].r/pdnraudio->frequeny_sum_count);
-  //   fprintf(pdnraudio->bFile,"%f ,", freq_data[j].i/pdnraudio->frequeny_sum_count);
-  // }
-  // fprintf(pdnraudio->aFile,"0};");
-  // fprintf(pdnraudio->bFile,"0};");
+#ifdef ESTIMATION_MODE
+  GstFFTF32Complex *freq_data = pdnraudio->frequeny_data;
+  fprintf (pdnraudio->aFile,"const gfloat estR[] = {");
+  for(int j=0; j<FFT_SIZE; j++) {
+#ifdef EST_MAX
+    fprintf (pdnraudio->aFile,"%f ,", freq_data[j].r);
+#else
+    fprintf (pdnraudio->aFile,"%f ,", freq_data[j].r/pdnraudio->frequeny_sum_count);
+#endif
+  }
+  fprintf (pdnraudio->aFile,"0};\n");
 
-  // fclose(pdnraudio->aFile);
-  // fclose(pdnraudio->bFile);
+  fprintf (pdnraudio->aFile,"const gfloat estI[] = {");
+  for(int j=0; j<FFT_SIZE; j++) {
+#ifdef EST_MAX
+    fprintf (pdnraudio->aFile,"%f ,", freq_data[j].i);
+#else
+    fprintf (pdnraudio->aFile,"%f ,", freq_data[j].i/pdnraudio->frequeny_sum_count);
+#endif
+  }
+  fprintf (pdnraudio->aFile,"0};");
 
-  g_free(pdnraudio->frequeny_data);
+  // fprintf (pdnraudio->bFile,"0};");
 
-  g_free(pdnraudio->in_array);
-  g_free(pdnraudio->out_array);
+  fclose (pdnraudio->aFile);
+  // fclose (pdnraudio->bFile);
+#endif
 
+  g_free (pdnraudio->frequeny_data);
+  g_free (pdnraudio->in_array);
+  g_free (pdnraudio->out_array);
+  g_free (pdnraudio->attack_count);
+  g_free (pdnraudio->release_count);
   gst_fft_f32_free (pdnraudio->fft_ctx);
-
 
   GST_DEBUG_OBJECT (pdnraudio, "finalize");
 
@@ -228,11 +246,17 @@ guint trans_count = 0;
 static void 
 fft_window_noise_reduction (GstPdNrAudio *pdnraudio) {
   GstFFTF32 *fft_ctx = pdnraudio->fft_ctx;
-  // GstFFTF32Complex *freq_data = pdnraudio->frequeny_data;
+#ifdef ESTIMATION_MODE
+  GstFFTF32Complex *freq_data = pdnraudio->frequeny_data;
+#endif
   GstFFTF32Complex *tmp_freq_data = g_new0 (GstFFTF32Complex, FFT_SIZE);
+  GstFFTF32Complex *mask_freq_data = g_new0 (GstFFTF32Complex, FFT_SIZE);
 
   gfloat *in_array = pdnraudio->in_array;
   gfloat *out_array = pdnraudio->out_array;
+
+  guint16 *attack_count = pdnraudio->attack_count;
+  guint16 *release_count = pdnraudio->release_count;
 
   gfloat *out_adata = g_new0 (gfloat, FFT_WINDOW_SIZE);
 
@@ -240,51 +264,124 @@ fft_window_noise_reduction (GstPdNrAudio *pdnraudio) {
     gfloat *frame_data = g_new0 (gfloat, FFT_WINDOW_SIZE);
     memcpy(frame_data, in_array+FFT_HOP_SIZE*i, FFT_WINDOW_SIZE*sizeof(gfloat));
 
-    // for(int j=0; j<FFT_WINDOW_SIZE; j++) {
-    //   fprintf(pdnraudio->aFile,"%f\n", frame_data[j]);
-    // }
-    // fprintf(pdnraudio->aFile,"**************\n");
-
     fft_ctx = gst_fft_f32_new (FFT_WINDOW_SIZE, FALSE);
 
     gst_fft_f32_window (fft_ctx, frame_data, GST_FFT_WINDOW_HANN);
     gst_fft_f32_fft (fft_ctx, frame_data, tmp_freq_data);
 
-    pdnraudio->frequeny_sum_count++;
-    // todo: manipulate frequency domain here
-    for(int j=0; j<FFT_SIZE; j++) {
-      // freq_data[j].r += fabs(tmp_freq_data[j].r);
-      // freq_data[j].i += fabs(tmp_freq_data[j].i);
-      gfloat fbs_freq_data_r = fabs(tmp_freq_data[j].r);
-      gfloat fbs_freq_data_i = fabs(tmp_freq_data[j].i);
-      if((fbs_freq_data_r <= estR[j]*F_GAIN)||(fbs_freq_data_i <= estR[j]*F_GAIN)) {
-        tmp_freq_data[j].r = 0;
-        tmp_freq_data[j].i = 0;
-      }
+    memcpy(mask_freq_data, tmp_freq_data, FFT_WINDOW_SIZE*sizeof(gfloat));
 
-      // if(j == 100)
-      //   g_print("r %f est %f \n", fabs(tmp_freq_data[j].r), estR[j]);
+    pdnraudio->frequeny_sum_count++;
+
+    guint16 freq_anr_count = 0;
+    gdouble freq_r_i_sum = 0.0;
+
+    for(int j=0; j<FFT_SIZE; j++) {
+#ifdef ESTIMATION_MODE
+#ifdef EST_MAX
+      if(fabs(tmp_freq_data[j].r) > freq_data[j].r)
+        freq_data[j].r = fabs(tmp_freq_data[j].r);
+      if(fabs(tmp_freq_data[j].i) > freq_data[j].i)
+        freq_data[j].i = fabs(tmp_freq_data[j].i);
+#else
+      freq_data[j].r += fabs(tmp_freq_data[j].r);
+      freq_data[j].i += fabs(tmp_freq_data[j].i);
+#endif
+#endif
+
+      // if(j>600) {
+      //   tmp_freq_data[j].r = 0;
+      //   tmp_freq_data[j].i = 0;
+      //   continue;
+      // }
+
+      gfloat fbs_freq_data_r = fabs (tmp_freq_data[j].r);
+      gfloat fbs_freq_data_i = fabs (tmp_freq_data[j].i);
+      gfloat fbs_freq_data_r_i = fbs_freq_data_r + fbs_freq_data_i;
+
+      // g_print ("freq=%d %f %f\n", j, fbs_freq_data_r_i, estR[j]+estI[j]);
+      if((fbs_freq_data_r_i) > ((estR[j]+estI[j])*F_GAIN)) {
+        gfloat subtractR = estR[j];
+        gfloat subtractI = estI[j];
+        subtractR = copysignf (subtractR, tmp_freq_data[j].r);
+        subtractI = copysignf (subtractI, tmp_freq_data[j].i);
+
+        if(NOISE_GATE) {
+          gfloat gGain = 1.0;
+          release_count[j] = 0;
+          if(attack_count[j] < ATTACK_STEPS) {
+            gGain = attack_gains[attack_count[j]];
+            attack_count[j]++;
+          }
+          // g_print("+ freq=%d (%f - %f) * %f ",j, tmp_freq_data[j].r, subtractR, gGain);
+          if(fabs(subtractR) > fbs_freq_data_r)
+            tmp_freq_data[j].r = 0;
+          else
+            tmp_freq_data[j].r = (tmp_freq_data[j].r - subtractR) * gGain;
+          if(fabs(subtractI) > fbs_freq_data_i)
+            tmp_freq_data[j].i = 0;
+          else
+            tmp_freq_data[j].i = (tmp_freq_data[j].i - subtractI) * gGain;
+          // g_print("= %f \n", tmp_freq_data[j].r);
+        }
+        else {
+          // g_print("+ freq=%d (%f - %f) ",j, tmp_freq_data[j].r, subtractR);
+          // g_print("= %f \n", tmp_freq_data[j].r);
+          tmp_freq_data[j].r -= subtractR;
+          tmp_freq_data[j].i -= subtractI;
+        }
+        freq_anr_count++;
+        freq_r_i_sum += tmp_freq_data[j].r;
+      }
+      else {
+        if(NOISE_GATE) {
+          gfloat gGain = 0;
+          attack_count[j] = 0;
+          if(release_count[j] < RELEASE_STEPS) {
+            gGain = release_gains[release_count[j]];
+            release_count[j]++;
+          }
+          tmp_freq_data[j].r *= gGain;
+          tmp_freq_data[j].i *= gGain;
+        }
+        else {
+          tmp_freq_data[j].r = 0;
+          tmp_freq_data[j].i = 0;
+        }
+      }
+#ifdef GUASSIAN_SMOOTH
+      gfloat gsGain = exp(-(pow(j,2)*pow(M_PI,2)/GUASSIAN_ALPHA));
+      tmp_freq_data[j].r *= gsGain;
+      tmp_freq_data[j].i *= gsGain;
+#endif
+#ifdef BACKGROUND_MASK
+      tmp_freq_data[j].r += mask_freq_data[j].r*0.001;
+      tmp_freq_data[j].i += mask_freq_data[j].i*0.001;
+#endif
     }
+
+    g_print ("freq_r_i_sum %lf %d\n", freq_r_i_sum, freq_anr_count);
+    // if((freq_r_i_sum <= 60) && (freq_anr_count <= 8)) {
+    //   g_free(tmp_freq_data);
+    //   tmp_freq_data = g_new0 (GstFFTF32Complex, FFT_SIZE);
+    // }
 
     gst_fft_f32_free (fft_ctx);
     fft_ctx = gst_fft_f32_new (FFT_WINDOW_SIZE, TRUE);
-    gst_fft_f32_inverse_fft(fft_ctx, tmp_freq_data, out_adata);
+    gst_fft_f32_inverse_fft (fft_ctx, tmp_freq_data, out_adata);
 
+    // Overlap add
     for(int j=0; j<FFT_WINDOW_SIZE; j++) {
       out_array[j+FFT_HOP_SIZE*i] += (out_adata[j]/FFT_WINDOW_SIZE);
     }
-    // fprintf(pdnraudio->aFile,"**************\n");
 
     gst_fft_f32_free (fft_ctx);
-    g_free(frame_data);
+    g_free (frame_data);
   }
 
-  // for(int j=0; j<FFT_WINDOW_SIZE; j++) {
-  //   fprintf(pdnraudio->aFile,"%f\n", out_array[j]);
-  // }
-
-  g_free(out_adata);
-  g_free(tmp_freq_data);
+  g_free (out_adata);
+  g_free (tmp_freq_data);
+  g_free (mask_freq_data);
 }
 
 /* transform */
@@ -293,10 +390,20 @@ gst_pd_nr_audio_transform (GstBaseTransform * trans, GstBuffer * inbuf,
     GstBuffer * outbuf)
 {
   GstPdNrAudio *pdnraudio = GST_PD_NR_AUDIO (trans);
-  // guint rate = GST_AUDIO_FILTER_RATE (pdnraudio);
-  // guint channels = GST_AUDIO_FILTER_CHANNELS (pdnraudio);
-  // guint bps = GST_AUDIO_FILTER_BPS (pdnraudio);
-  // guint bpf = GST_AUDIO_FILTER_BPF (pdnraudio);
+  guint rate = GST_AUDIO_FILTER_RATE (pdnraudio);
+  guint channels = GST_AUDIO_FILTER_CHANNELS (pdnraudio);
+  guint bps = GST_AUDIO_FILTER_BPS (pdnraudio);
+  guint bpf = GST_AUDIO_FILTER_BPF (pdnraudio);
+
+  trans_count++;
+#ifdef ESTIMATION_MODE
+  if(trans_count == 500) {
+    g_free (pdnraudio->frequeny_data);
+    g_print ("frequeny_data clean\n");
+    trans_count = 0;
+    pdnraudio->frequeny_data = g_new (GstFFTF32Complex, FFT_SIZE);
+  }
+#endif
 
   guint8 *in_data, *out_data;
   gsize in_size, out_size;
@@ -319,34 +426,34 @@ gst_pd_nr_audio_transform (GstBaseTransform * trans, GstBuffer * inbuf,
   out_data = oinfo.data;
   out_size = oinfo.size;
 
-  trans_count++;
-  // g_print("nr_transform %d buffer size %ld rate %d channels %d bps %d bpf %d\n",
-   // trans_count, in_size, rate, channels, bps, bpf);
+  if(in_size != FFT_WINDOW_SIZE) {
+    memcpy (out_data, in_data, in_size);
+    gst_buffer_unmap (inbuf, &iinfo);
+    gst_buffer_unmap (outbuf, &oinfo);
+    return GST_FLOW_OK;
+  }
+  
+  // g_print("nr_transform %d buffer size %ld:%ld rate %d channels %d bps %d bpf %d\n",
+  //  trans_count, in_size, out_size, rate, channels, bps, bpf);
 
-  memcpy(in_array+FFT_WINDOW_SIZE, in_data, in_size);
+  memcpy (in_array+FFT_WINDOW_SIZE, in_data, in_size);
 
-  fft_window_noise_reduction(pdnraudio);
+  fft_window_noise_reduction (pdnraudio);
 
   memcpy(out_data, out_array, in_size);
 
   gfloat *tmp_out_array = g_new0 (gfloat, FFT_WINDOW_SIZE);
 
-  // if(trans_count == 30) {
-  //   for(int j=0; j<FFT_WINDOW_SIZE*2; j++) {
-  //     fprintf(pdnraudio->aFile,"%f\n", out_array[j]);
-  //   }
-  // }
+  memcpy (tmp_out_array, out_array+FFT_WINDOW_SIZE, FFT_WINDOW_SIZE*sizeof(gfloat));
+  memset (out_array+FFT_WINDOW_SIZE, 0, FFT_WINDOW_SIZE*sizeof(gfloat));
+  memcpy (out_array, tmp_out_array, FFT_WINDOW_SIZE*sizeof(gfloat));
 
-  memcpy(tmp_out_array, out_array+FFT_WINDOW_SIZE, FFT_WINDOW_SIZE*sizeof(gfloat));
-  memset(out_array+FFT_WINDOW_SIZE, 0, FFT_WINDOW_SIZE*sizeof(gfloat));
-  memcpy(out_array, tmp_out_array, FFT_WINDOW_SIZE*sizeof(gfloat));
+  g_free (tmp_out_array);
 
-  g_free(tmp_out_array);
+  memcpy (in_array, in_data, in_size);
 
-  memcpy(in_array, in_data, in_size);
-
-  gst_buffer_unmap(inbuf, &iinfo);
-  gst_buffer_unmap(outbuf, &oinfo);
+  gst_buffer_unmap (inbuf, &iinfo);
+  gst_buffer_unmap (outbuf, &oinfo);
 
   return GST_FLOW_OK;
 }
@@ -358,7 +465,7 @@ gst_pd_nr_audio_transform_ip (GstBaseTransform * trans, GstBuffer * buf)
 
   GST_DEBUG_OBJECT (pdnraudio, "transform_ip");
 
-  g_print("gst_pd_nr_audio_transform_ip\n");
+  g_print ("gst_pd_nr_audio_transform_ip\n");
 
   return GST_FLOW_OK;
 }
@@ -396,6 +503,6 @@ GST_PLUGIN_DEFINE (
     GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     pdnraudio,
-    "PlayDate audio noise reduction plugin description",
+    "PlayDate audio noise reduction plugin",
     plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
 
